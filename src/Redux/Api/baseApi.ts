bffb560cf6fc9f647e3d719fc0fdcb3bd8616e28
ismailemayej <salikbd.com@gmail.com>
2024-03-26 00:19:13 +0600

    live server site link added

--- src/Redux/Api/baseApi.ts
+++ src/Redux/Api/baseApi.ts
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      "https://relief-management-server-site-k03iy52om-ismailemayejs-projects.vercel.app/",
+    baseUrl: "https://relief-management-server-site.vercel.app",
   }),
   endpoints: () => ({}),
   tagTypes: ["homepost", "supply"],
